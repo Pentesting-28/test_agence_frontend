b6792f49ebab8652a08e110c94ff67c9d2e11a0c
Pentesting-28 <mpenav28@gmail.com>
2022-08-12 12:41:17 -0400

    test cors

--- src/store/modules/consultant/consultant.js
+++ src/store/modules/consultant/consultant.js
@@ -39,16 +39,39 @@ export const consultant = {
                 //     "Content-Type": "application/json",
                 //     "X-localization": "es"
                 // }
-                const response = await fetch("https://consultanttestagence.000webhostapp.com/api/v1/consultant")
-                // .then(response => response.json())
-                // .then(data => (commit("SET_CONSULTANTS", data)));
+                // const response = await fetch("http://agenceconsultant.42web.io/api/v1/consultant",{
+                //     statusCode: 200,
+                //     headers: {
+                //         "Access-Control-Allow-Headers" : "Content-Type",
+                //         "Access-Control-Allow-Origin": "*",
+                //         "Access-Control-Allow-Methods": "POST,GET"
+                //     },
+                // });
+                // // .then(response => response.json())
+                // // .then(data => (commit("SET_CONSULTANTS", data)));
+                // if (!response.ok) {
+                //     throw new Error(`HTTP error! status: ${response.status}`);
+                // }
+
+                // const data = await response.json();
+                const url = "http://agenceconsultant.42web.io/api/v1/consultant/";
+                const headers = {
+                    "Access-Control-Allow-Origin": "http://172.16.31.43:8080/",
+                    'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept'
+                }
+                
+                const response = await fetch(url, {
+                    method : "GET",
+                    // mode: "cors",
+                    headers: headers
+                });
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const data = await response.json();
-                console.log(data)
-                commit("SET_CONSULTANTS", data)
-                return data
+                console.log(data);
+                commit("SET_CONSULTANTS", data);
+                return data;
             } catch (error) {
                 return Promise.reject(error);
             }
@@ -69,11 +92,15 @@ export const consultant = {
                 // }
 
                 //  await fetch("https://consultanttestagence.000webhostapp.com/api/v1/consultant", options);
-                const response = await fetch("https://consultanttestagence.000webhostapp.com/api/v1/consultant", {
+                const response = await fetch("http://agenceconsultant.42web.io/api/v1/consultant/", {
                     method: 'POST',
                     headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin' : 'http://172.16.31.43:8080',
+                        // 'Access-Control-Allow-Methods':'POST, GET',
+                        'Accept':'application/json',
+                        'content-type':'application/json',
+                        // 'Access-Control-Allow-Headers': "Origin, X-Api-Key, X-Requested-With, Access-Control-Allow-Headers, Content-Type, Accept, Authorization",
+                        // 'cache-control': 'no-cache'
                     },
                     body: JSON.stringify(payload)
                 } );
